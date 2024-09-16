e6ab824dc4a100c63243fb59dfe292950d5009b4
mskalbania <mail@skalbania.it>
2024-09-16 16:57:32 +0200

    Make db conn configurable, minor improvements

--- authorization-server/main.go
+++ authorization-server/main.go
@@ -15,27 +15,29 @@ import (
 const ADDRESS = ":8080"
 
 func main() {
-	key := os.Getenv("JWT_SIGNING_KEY")
-	if key == "" {
+	jwtSignKey := os.Getenv("JWT_SIGNING_KEY")
+	if jwtSignKey == "" {
 		log.Fatalf("JWT_SIGNING_KEY not set")
 	}
+	dbConnString := os.Getenv("DB_CONN_STRING")
+	if dbConnString == "" {
+		log.Fatalf("DB_CONN_STRING not set")
+	}
+	userDb := db.NewPostgresDb(dbConnString)
+	userAPI := api.NewAuthorizationAPI(userDb, api.JWTProperties{
+		SigningKey:           []byte(jwtSignKey),
+		AccessTokenDuration:  15 * time.Minute,
+		RefreshTokenDuration: 24 * time.Hour,
+	})
 
 	lis, err := net.Listen("tcp", ADDRESS)
 	if err != nil {
 		log.Fatalf("error starting server: %v", err)
 	}
-	//TODO: make the connection string configurable
-	userDb := db.NewPostgresDb("postgresql://postgres:postgres@postgres:5432/postgres")
-	userAPI := api.NewAuthorizationAPI(userDb, api.JWTProperties{
-		SigningKey:           []byte(key),
-		AccessTokenDuration:  15 * time.Minute,
-		RefreshTokenDuration: 24 * time.Hour,
-	})
-
 	s := grpc.NewServer()
 	auth.RegisterAuthorizationServer(s, userAPI)
 
-	//for debugging purposes, allows clients to query for available services, types etc.
+	//for debugging purposes, reflection allows (generic) clients to query for available services, types etc.
 	reflection.Register(s)
 
 	log.Println("starting server on", ADDRESS)
